782eb1641a80e19024988b9b776e5575a7254ba4
tortoise1992 <772084015@qq.com>
2018-02-24 17:44:41 +0800

    111

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,10 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.post('/api',function(req, res, next){
+  //微信得到返回后会通过你的认证
+	var query = req.query;    
+	var echostr = query.echostr; 
+	res.send(echostr);
+})
 module.exports = router;
