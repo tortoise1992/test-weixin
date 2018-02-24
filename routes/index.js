e4f7350667cf3eb060b70ea51be18be138b8497a
tortoise1992 <772084015@qq.com>
2018-02-25 07:48:49 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
-
+var sha1 = require('sha1');
+var config = {
+	wechat:{
+			appID:'wxf500fe4e63922a0f', //填写你自己的appID
+			appSecret:'a9e831c399987fe7fa2dfbe2c2deb789',  //填写你自己的appSecret
+			token:'ahui'  //填写你自己的token
+	}
+};
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  	var token = config.wechat.token;
+    var signature = req.query.signature;
+    var nonce = req.query.nonce;
+    var timestamp = req.query.timestamp;
+    var echostr = req.query.echostr;
+    var str = [token,timestamp,nonce].sort().join(''); //按字典排序，拼接字符串
+		var sha = sha1(str); //加密
+		res.send((sha === signature)? echostr + '' : 'failed')    
 });
 router.post('/api',function(req, res, next){
   //微信得到返回后会通过你的认证
